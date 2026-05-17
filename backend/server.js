4c9c0c59b8313bffa9d9f2fce3d5fa31fa024c6f
SofijaDjordjevic <sofija.djordjevic98@gmail.com>
2026-05-17 12:09:54 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorHandler.js';
 
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
